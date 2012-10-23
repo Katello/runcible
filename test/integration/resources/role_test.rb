35697af2b4a193c41bcfc4583ab59008a1242343
Eric D. Helms <ehelms@redhat.com>
2012-10-23 13:57:55 -0400

    Adding back previously deleted test files.

--- test/integration/resources/role_test.rb
+++ test/integration/resources/role_test.rb
@@ -9,4 +9,54 @@
 # have received a copy of GPLv2 along with this software; if not, see
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 
+require 'rubygems'
+require 'minitest/autorun'
 
+require './lib/runcible/resources/user'
+require './lib/runcible/resources/role'
+
+
+class TestResourcesRoles < MiniTest::Unit::TestCase
+  def setup
+    @username = "integration_test_user"
+    @role_name = "super-users"
+    @resource = Runcible::Resources::Role
+
+    VCR.use_cassette('pulp_user') do
+      Runcible::Resources::User.create(@username)
+    end
+
+    VCR.insert_cassette('pulp_role')
+  end
+
+  def teardown
+    VCR.use_cassette('pulp_user') do
+      Runcible::Resources::User.delete(@username)
+    end
+
+    VCR.eject_cassette
+  end
+
+  def test_path_without_role_name
+    path = @resource.path
+    assert_match("roles/", path)
+  end
+
+  def test_path_with_role_name
+    path = @resource.path(@role_name)
+    assert_match("roles/#{@role_name}", path)
+  end
+
+  def test_add
+    response = @resource.add(@role_name, @username)
+    assert response.code == 200
+    @resource.remove(@role_name, @username)
+  end
+
+  def test_remove
+    @resource.add(@role_name, @username)
+    response = @resource.remove(@role_name, @username)
+    assert response.code == 200
+  end
+
+end
--- test/integration/resources/task_test.rb
+++ test/integration/resources/task_test.rb
@@ -9,4 +9,71 @@
 # have received a copy of GPLv2 along with this software; if not, see
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 
+require 'rubygems'
+require 'minitest/autorun'
 
+require './test/integration/resources/helpers/repository_helper'
+require './lib/runcible/resources/task'
+
+
+module TestResourcesTaskBase
+  include RepositoryHelper
+
+  def setup
+    @resource = Runcible::Resources::Task
+    VCR.insert_cassette('pulp_task')
+  end
+
+  def teardown
+    VCR.eject_cassette
+  end
+
+end
+
+
+class TestResourcesTask < MiniTest::Unit::TestCase
+  include TestResourcesTaskBase
+
+  def setup
+    super
+    RepositoryHelper.create_repo(:importer => true)
+    RepositoryHelper.sync_repo(false)
+  end
+
+  def teardown
+    super
+    RepositoryHelper.destroy_repo
+  end
+
+  def test_path
+    path = @resource.path
+    assert_match("tasks/", path)
+  end
+
+  def test_path_with_task_id
+    path = @resource.path(RepositoryHelper.task['task_id'])
+    assert_match("tasks/#{RepositoryHelper.task['task_id']}/", path)
+  end
+
+  def test_poll
+    response = @resource.poll(RepositoryHelper.task['task_id'])
+    assert response.code == 200
+    assert response['task_id'] == RepositoryHelper.task['task_id']
+  end
+
+  def test_list
+    response = @resource.list
+    assert response.code == 200
+    assert response.length > 0
+    #assert response.first['task_id'] == RepositoryHelper.task['task_id']
+  end
+
+=begin
+  #TODO: Needs more reliable testable scenario - scheduled sync in the future?
+  def test_cancel
+    response = @resource.cancel(RepositoryHelper.task['task_id'])
+    assert response.code == 200
+  end
+=end
+
+end
