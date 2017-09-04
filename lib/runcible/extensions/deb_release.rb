061e78de96cc50ba300e32f44928c014c4867b62
Matthias Dellweg <dellweg@atix.de>
2017-09-04 19:28:27 +0200

    refs #10478 - Add deb extensions

---
+++ lib/runcible/extensions/deb_release.rb
@@ -0,0 +1,24 @@
+module Runcible
+  module Extensions
+    class DebRelease < Runcible::Extensions::Unit
+      def self.content_type
+        'deb_release'
+      end
+
+      # This function is not implemented for Releases since they do not have content IDs
+      def find
+        fail NotImplementedError
+      end
+
+      # This function is not implemented for Releases since they do not have content IDs
+      def find_all
+        fail NotImplementedError
+      end
+
+      # This function is not implemented for Releases since they do not have content IDs
+      def unassociate_ids_from_repo(repo_id, ids)
+        fail NotImplementedError
+      end
+    end
+  end
+end
