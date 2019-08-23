5bc149a687356f36a297339ecb41dea01ef5d5e0
Ian Ballou <ianballou67@gmail.com>
2019-08-23 16:36:45 +0000

    Add support for environment groups

---
+++ lib/runcible/extensions/package_environment.rb
@@ -0,0 +1,9 @@
+module Runcible
+  module Extensions
+    class PackageEnvironment < Runcible::Extensions::Unit
+      def self.content_type
+        'package_environment'
+      end
+    end
+  end
+end
